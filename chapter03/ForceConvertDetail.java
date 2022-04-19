ec79113e6dfea486a68767e2a76b2b1f07314559
tom_pro <2961804169@qq.com>
2022-04-19 09:46:05 +0800

    second

---
+++ chapter03/ForceConvertDetail.java
@@ -0,0 +1,5 @@
+public class ForceConvertDetail{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ chapter03/StringToBasic.java
@@ -0,0 +1,6 @@
+public class StringToBasic {
+    public static void main(String[] args) {
+            int n1 =100;
+            System.out.println(n1);
+        }
+}
